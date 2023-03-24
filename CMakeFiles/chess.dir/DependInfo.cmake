e5f6a315ff84e79e419c31ebb56abb48a361da28
Arne De Peuter <arne@depeuter.org>
2023-03-24 18:50:27 +0100

    perft fix

---
+++ CMakeFiles/chess.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/AI.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/AI.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/BitboardHandler.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/BitboardHandler.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Chess.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Chess.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/ChessStack.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/ChessStack.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Interacter.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Interacter.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/Bishop.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/Bishop.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/ChessPiece.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/ChessPiece.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/King.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/King.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/Knight.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/Knight.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/Pawn.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/Pawn.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/Queen.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/Queen.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/Rook.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/Rook.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/Pieces/SlidingPiece.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/Pieces/SlidingPiece.cpp.o"
+  "/mnt/c/Users/arne/Documents/2022-2023/chess/src/main.cpp" "/mnt/c/Users/arne/Documents/2022-2023/chess/CMakeFiles/chess.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "include/boost_1_81_0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess.dir/src/AI.cpp.o"
+  "CMakeFiles/chess.dir/src/BitboardHandler.cpp.o"
+  "CMakeFiles/chess.dir/src/Chess.cpp.o"
+  "CMakeFiles/chess.dir/src/ChessStack.cpp.o"
+  "CMakeFiles/chess.dir/src/Interacter.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/Bishop.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/ChessPiece.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/King.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/Knight.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/Pawn.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/Queen.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/Rook.cpp.o"
+  "CMakeFiles/chess.dir/src/Pieces/SlidingPiece.cpp.o"
+  "CMakeFiles/chess.dir/src/main.cpp.o"
+  "chess"
+  "chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
